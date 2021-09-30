0f311f61608ae9368f2b9a364ad22b04a457b6b9
Andrew <dandrewbarlow@gmail.com>
2021-09-30 12:10:32 -0600

    added sculpture render, fixed css stuff

--- js/jquery-handling.js
+++ js/jquery-handling.js
@@ -1,4 +1,4 @@
-let content = $("#information");
+let content = $("#content");
 
 let returnButtonString = '<button id="return" onClick="clearContent()">Return to Visual</button>';
 
@@ -7,7 +7,7 @@ let returnButton = $('#return');
 $("#about").on("click", () =>{
     content.html( returnButtonString + 
     `
-    <p>
+    <p id="information">
         <em>Emitting</em> is an immersive art installation utilizing projection mapping and interactive design to explore loneliness and isolation. It is being made by Andrew Barlow, Madi Heath, Phu Le, and Sofi Ozambela as their Creative Technology and Design Capstone Project.
     </p>
     `
@@ -17,7 +17,7 @@ $("#about").on("click", () =>{
 $("#when").on("click", () => {
     content.html( returnButtonString + 
     `
-    <p>
+    <p id="information">
         Emitting will be open to the public on November 19th & 20th, in The Black Box Experimental Studio, located in the second basement level of <a href="https://map.concept3d.com/?id=336#!m/193967">CU Boulder's Roser Atlas building</a>.
     </p>
     `
