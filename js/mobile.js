d5b364a9b84bc9bdf24910b9101f8950b99e814a
Andrew <dandrewbarlow@gmail.com>
2021-10-30 14:03:46 -0600

    mobile optimization & fixes

---
+++ js/mobile.js
@@ -0,0 +1,25 @@
+// Prevent Scrolling
+// https://stackoverflow.com/questions/10592411/disable-scrolling-in-all-mobile-devices
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
+
+// Get *correct* viewport height
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
