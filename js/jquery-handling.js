b99daa750b67f98d32a4e5939765dd5516fe5e6c
Andrew Barlow <dandrewbarlow@gmail.com>
2021-11-09 16:42:21 -0700

    added opening time

--- js/jquery-handling.js
+++ js/jquery-handling.js
@@ -18,7 +18,7 @@ $("#when").on("click", () => {
     content.html( returnButtonString + 
     `
     <p id="information">
-        Emitting will be open to the public on November 19th & 20th, in The Black Box Experimental Studio, located in the second basement level of <a href="https://map.concept3d.com/?id=336#!m/193967">CU Boulder's Roser Atlas building</a>.
+        Emitting will be open to the public on November 19th & 20th, with a special opening at 5pm on the 19th, in The Black Box Experimental Studio, located in the second basement level of <a href="https://map.concept3d.com/?id=336#!m/193967">CU Boulder's Roser Atlas building</a>.
     </p>
     `
     )
